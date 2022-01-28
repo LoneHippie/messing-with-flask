d599fa08b23de58435dec53d8d3b2659cf7c9c7b
LoneHippie <lonehippie.music@gmail.com>
2022-01-28 12:00:54 +0200

    added typing and reformatted endpoints

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import null
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
@@ -9,30 +10,33 @@ db = SQLAlchemy(app)
 base_url = '/api/v1'
 
 class Drink(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), unique=True, nullable=False)
-    description = db.Column(db.String(240))
+    id: int = db.Column(db.Integer, primary_key=True)
+    name: str = db.Column(db.String(80), unique=True, nullable=False)
+    description: str = db.Column(db.String(240))
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f"{self.name} - {self.description}"
 
 @app.route('/')
-def index():
-    return 'yo man'
+def index() -> str:
+    return 'Test Flask API home route'
 
 @app.route(f'{base_url}/drinks', methods=['GET', 'POST'])
-def handle_req():
+def handle_req() -> dict:
     if request.method == 'GET':
-        drinks = Drink.query.all()
+        drinks: list[Drink] = Drink.query.all()
         res = []
         for drink in drinks:
-            entry = {'name': drink.name, 'description': drink.description}
+            entry = {
+                'name': drink.name, 
+                'description': drink.description
+            }
             res.append(entry)
         return {
             "drinks": res
         }
     elif request.method == 'POST':
-        drink = Drink(
+        drink: Drink = Drink(
             name=request.json['name'],
             description=request.json['description']
         )
@@ -45,13 +49,15 @@ def handle_req():
         }
 
 @app.route(f'{base_url}/drinks/<id>', methods=['GET', 'DELETE'])
-def get_drink(id):
+def get_drink(id) -> dict:
     
-    drink = Drink.query.get_or_404(id)
+    drink: Drink or None = Drink.query.get_or_404(id)
 
     if request.method == 'GET':
         if drink is None:
-            return { 'status': 'not found' }
+            return { 
+                'status': 'not found' 
+            }
         else:
             return jsonify({
                 'name': drink.name,
@@ -59,11 +65,15 @@ def get_drink(id):
             })
     elif request.method == 'DELETE':
         if drink is None:
-            return { 'status': 'not found' }
+            return { 
+                'status': 'not found'
+            }
         else:
             db.session.delete(drink)
             db.session.commit()
-            return { 'status': 'success' }
+            return { 
+                'status': 'success'
+            }
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
