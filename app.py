4cbf087708d400f352f9f3d796454aef3e4235c5
LoneHippie <lonehippie.music@gmail.com>
2022-01-28 12:01:48 +0200

    removed accidental null import

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import null
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
